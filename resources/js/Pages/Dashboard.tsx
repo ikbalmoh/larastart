3369338985e57e10b92b7c9757a60993790bae72
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-02-23 21:31:46 +0700

    layout: disable dark mode

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -7,7 +7,7 @@ export default function Dashboard() {
             <Head title="Dashboard" />
 
             <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div className="p-6 text-gray-900">You're logged in!</div>
+                <div className="p-6 text-gray-900 ">You're logged in!</div>
             </div>
         </AuthenticatedLayout>
     );
