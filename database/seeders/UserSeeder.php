3f82ac76ac67e402c3f367ae3d12c6b3767a0660
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-02-23 09:47:02 +0700

    role

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create(['name' => 'admin']);
+        $users = User::factory()->count(2)->create();
+        foreach ($users as $key => $user) {
+            $user->assignRole('admin');
+        }
+    }
+}
