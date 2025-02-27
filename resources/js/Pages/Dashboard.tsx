87dc0476adbb3b90b3a78533fb4fb4c9d025f371
Ikbal Moh <hikmat.iqbal@gmail.com>
2025-02-27 23:24:37 +0700

    dark scheme

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -6,8 +6,8 @@ export default function Dashboard() {
         <AuthenticatedLayout>
             <Head title="Dashboard" />
 
-            <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div className="p-6 text-gray-900 ">You're logged in!</div>
+            <div className="overflow-hidden bg-white dark:bg-neutral-900 shadow-sm sm:rounded-lg">
+                <div className="p-6 text-gray-900 dark:text-slate-100">You're logged in!</div>
             </div>
         </AuthenticatedLayout>
     );
